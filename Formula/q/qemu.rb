39d4ebcaff1adee86a59fa9736241e64ff1d0c65
Klaus Frank <2544867+agowa@users.noreply.github.com>
2024-05-17 17:09:09 +0200

    Update qemu.rb

--- Formula/q/qemu.rb
+++ Formula/q/qemu.rb
@@ -47,7 +47,7 @@ class Qemu < Formula
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
-  depends_on "maciasl" => [cask:, :build]
+  depends_on "maciasl" => [:cask, :build]
 
   on_linux do
     depends_on "attr"
