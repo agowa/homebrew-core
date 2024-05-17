0e02e3b0c823f36f4ac5ce70e4dcc4329f7f7dfa
Klaus Frank <2544867+agowa@users.noreply.github.com>
2024-05-17 16:12:17 +0200

    Update qemu.rb

--- Formula/q/qemu.rb
+++ Formula/q/qemu.rb
@@ -26,7 +26,6 @@ class Qemu < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "spice-protocol" => :build
-  depends_on "attr" => :build
   depends_on "gtk+3" => :build
   depends_on "libcap-ng" => :build
 
